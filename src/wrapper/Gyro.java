ba28e26868ebd131c0ad0a3f6b85af7dbf3ea083
Nicholas264 <nicholaspetrone12@gmail.com>
2018-02-01 16:22:54 -0800

    Added updated Files

---
+++ src/wrapper/Gyro.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team4738.robot;
+import edu.wpi.first.wpilibj.AnalogGyro;
+
+ public class Gyro {
+	public AnalogGyro gyro;
+	MovingAverage average;
+	double offset= 0;
+	
+	public Gyro(int port){
+		//gyro = new ADXRS450_Gyro(Port.kMXP);
+		gyro = new AnalogGyro(0);
+		gyro.reset();
+		average = new MovingAverage(5);
+	}
+	
+	public double getAngle(){
+		double angle = gyro.getAngle();
+		
+		return angle;//average.average(angle);
+		//angle =(Math.abs(angle / 360) - (int) (Math.abs(angle / 360))); //ASK JACOB what the "int" portion of the code does
+		//return average.average(angle * 360);
+		
+	}
+	
+	public double getOffsetAngle(){
+		return getAngle() + offset;
+	}
+	
+	public void reset(){
+		offset += getAngle();
+		gyro.reset();
+	}
+}
