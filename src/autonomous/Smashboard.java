d988c32fbc3ced05337a25657864f8387f5bf808
Wynter-Tau <33078460+Wynter-Tau@users.noreply.github.com>
2018-02-17 20:15:12 -0800

    added position to dash
    updates so that left is left and right is right

--- src/autonomous/Smashboard.java
+++ src/autonomous/Smashboard.java
@@ -15,5 +15,13 @@ public class Smashboard {
 		String data = (pos.getX() + smashboardHeight) + "," + pos.getY() + "," + pos.getTheta();
 		SmartDashboard.putString("destination", data);
 	}
-	
+	public static Position getRobotStartPos(){
+		double startPos = SmartDashboard.getNumber("start", 0);
+		Position robotPos = new Position(115 * startPos, 19);
+		if (startPos == 0) {
+			robotPos.setX(5);
+		}
+		return robotPos;
+		
+	}
 }
