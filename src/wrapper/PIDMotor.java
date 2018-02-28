a11f6654fda3ede99385534fe557e39dd1dab43e
Jacob Golden-Needham <therealbunn1e5@gmail.com>
2018-02-27 19:32:56 -0800

    PIDTEST

---
+++ src/wrapper/PIDMotor.java
@@ -0,0 +1,22 @@
+package wrapper;
+
+import edu.wpi.first.wpilibj.*;
+
+public class PIDMotor {
+	
+	public PIDController controller;
+	public PWMSpeedController motorController;
+	public Encoder encoder;
+	
+	public PIDMotor(PWMSpeedController motorController, Encoder encoder, PIDController controller, boolean isInverted) {
+		this.motorController = motorController;
+		this.encoder = encoder;
+		this.controller = controller;
+		motorController.setInverted(isInverted);
+	}
+	
+	public void set(double speed) {
+		motorController.set(speed + controller.calcP(encoder.getSpeed() / Constants.MAX_SPEED, speed));
+	}
+
+}
